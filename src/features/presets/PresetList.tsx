afdb7b7cf9603fad3a4a6bee72ad62a720f1bc82
Kim Rejstrom <kim.rejstrom@futurice.com>
2020-01-08 09:58:20 +0200

    Add carousel to presetList

--- src/features/presets/PresetList.tsx
+++ src/features/presets/PresetList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import battleAxe from 'features/presets/battle_axe.svg';
 import shield from 'features/presets/shield.svg';
 import sword from 'features/presets/sword.svg';
+import Carousel, { CarouselProps } from 'nuka-carousel';
 import { Preset } from 'features/presets/PresetFeature';
 
 export const PresetList: React.FC<{ presets: Preset[] }> = ({ presets }) => {
@@ -29,7 +30,7 @@ export const PresetList: React.FC<{ presets: Preset[] }> = ({ presets }) => {
     return (
       <div
         key={index}
-        className={`${bgColor} m-1 relative overflow-hidden rounded-lg w-40 shadow-lg`}
+        className={`${bgColor} m-1 relative overflow-hidden rounded-lg shadow-lg`}
       >
         <svg
           className="absolute bottom-0 left-0 mb-2"
@@ -70,7 +71,7 @@ export const PresetList: React.FC<{ presets: Preset[] }> = ({ presets }) => {
             alt=""
           />
           <div className="block absolute">
-            <span className="text-xl text-white font-bold">
+            <span className="text-xl text-white font-bold text-shadow">
               {preset.formula}
             </span>
           </div>
@@ -94,150 +95,18 @@ export const PresetList: React.FC<{ presets: Preset[] }> = ({ presets }) => {
       </div>
     );
   });
+
+  const carouselSettings: CarouselProps = {
+    wrapAround: true,
+    slidesToShow: 2,
+    cellAlign: 'center',
+    transitionMode: 'scroll3d',
+    withoutControls: true,
+  };
+
   return (
-    <div className="flex flex-wrap items-center justify-center p-4">
-      {renderedPresets}
+    <div className="flex flex-wrap items-center justify-center p-4 max-w-md m-auto">
+      <Carousel {...carouselSettings}>{renderedPresets}</Carousel>
     </div>
   );
 };
-
-//     <div className="flex-shrink-0 m-6 relative overflow-hidden bg-orange-500 rounded-lg max-w-xs shadow-lg">
-//       <svg
-//         className="absolute bottom-0 left-0 mb-8"
-//         viewBox="0 0 375 283"
-//         fill="none"
-//         style={{ transform: 'scale(1.5)', opacity: 0.1 }}
-//       >
-//         <rect
-//           x="159.52"
-//           y="175"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 159.52 175)"
-//           fill="white"
-//         />
-//         <rect
-//           y="107.48"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 0 107.48)"
-//           fill="white"
-//         />
-//       </svg>
-//       <div className="relative pt-10 px-10 flex items-center justify-center">
-//         <div
-//           className="block absolute w-48 h-48 bottom-0 left-0 mb-24 ml-3"
-//           style={{
-//             background: 'radial-gradient(black, transparent 60%)',
-//             transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
-//             opacity: 0.2,
-//           }}
-//         ></div>
-//         <img className="relative w-40" src={shield} alt="" />
-//       </div>
-//       <div className="relative text-white px-6 pb-6 mt-6">
-//         <span className="block opacity-75 -mb-1">Indoor</span>
-//         <div className="flex justify-between">
-//           <span className="block font-semibold text-xl">Peace Lily</span>
-//           <span className="block bg-white rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-//             $36.00
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="flex-shrink-0 m-6 relative overflow-hidden bg-teal-500 rounded-lg max-w-xs shadow-lg">
-//       <svg
-//         className="absolute bottom-0 left-0 mb-8"
-//         viewBox="0 0 375 283"
-//         fill="none"
-//         style={{ transform: 'scale(1.5)', opacity: 0.1 }}
-//       >
-//         <rect
-//           x="159.52"
-//           y="175"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 159.52 175)"
-//           fill="white"
-//         />
-//         <rect
-//           y="107.48"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 0 107.48)"
-//           fill="white"
-//         />
-//       </svg>
-//       <div className="relative pt-10 px-10 flex items-center justify-center">
-//         <div
-//           className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-//           style={{
-//             background: 'radial-gradient(black, transparent 60%)',
-//             transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
-//             opacity: 0.2,
-//           }}
-//         ></div>
-//         <img className="relative w-40" src={battleAxe} alt="" />
-//       </div>
-//       <div className="relative text-white px-6 pb-6 mt-6">
-//         <span className="block opacity-75 -mb-1">Outdoor</span>
-//         <div className="flex justify-between">
-//           <span className="block font-semibold text-xl">Monstera</span>
-//           <span className="block bg-white rounded-full text-teal-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-//             $45.00
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//     <div className="flex-shrink-0 m-6 relative overflow-hidden bg-purple-500 rounded-lg max-w-xs shadow-lg">
-//       <svg
-//         className="absolute bottom-0 left-0 mb-8"
-//         viewBox="0 0 375 283"
-//         fill="none"
-//         style={{ transform: 'scale(1.5)', opacity: 0.1 }}
-//       >
-//         <rect
-//           x="159.52"
-//           y="175"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 159.52 175)"
-//           fill="white"
-//         />
-//         <rect
-//           y="107.48"
-//           width="152"
-//           height="152"
-//           rx="8"
-//           transform="rotate(-45 0 107.48)"
-//           fill="white"
-//         />
-//       </svg>
-//       <div className="relative pt-10 px-10 flex items-center justify-center">
-//         <div
-//           className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
-//           style={{
-//             background: 'radial-gradient(black, transparent 60%)',
-//             transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
-//             opacity: 0.2,
-//           }}
-//         ></div>
-//         <img className="relative w-40" src={sword} alt="" />
-//       </div>
-//       <div className="relative text-white px-6 pb-6 mt-6">
-//         <span className="block opacity-75 -mb-1">Outdoor</span>
-//         <div className="flex justify-between">
-//           <span className="block font-semibold text-xl">Oak Tree</span>
-//           <span className="block bg-white rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">
-//             $68.50{' '}
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
