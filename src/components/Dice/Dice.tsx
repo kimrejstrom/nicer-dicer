70f40c97c1ab55b1b355b34c9b04d556c5496066
Kim Rejstrom <kim.rejstrom@futurice.com>
2020-01-11 16:43:27 +0200

    Add dice

---
+++ src/components/Dice/Dice.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import d20 from 'images/d20.svg';
+import d12 from 'images/d12.svg';
+import d10 from 'images/d10.svg';
+import d8 from 'images/d8.svg';
+import d6 from 'images/d6.svg';
+import d4 from 'images/d4.svg';
+
+export const Dice: React.FC = () => {
+  return (
+    <div className="flex align-center items-center justify-between w-64 mx-auto mb-4">
+      <img src={d4} className="w-10 px-2 py-2" alt="logo" />
+      <img src={d6} className="w-10 px-2 py-2" alt="logo" />
+      <img src={d8} className="w-10 px-2 py-2" alt="logo" />
+      <img src={d10} className="w-10 px-2 py-2" alt="logo" />
+      <img src={d12} className="w-10 px-2 py-2" alt="logo" />
+      <img src={d20} className="w-10 px-2 py-2" alt="logo" />
+    </div>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Dice } from 'components/Dice/Dice';
 
 export const Home: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ export const Home: React.FC = () => {
         <div className="text-white text-xl pb-4">
           The Ultimate RPG Dice Roller
         </div>
+        <Dice />
         <p className="mx-auto w-64 mb-6">
           The dice roller is very easy to use, simply type in the formula in the
           box and press enter. Nicer Dicer features support for a wide array of
@@ -17,6 +19,7 @@ export const Home: React.FC = () => {
           </Link>{' '}
           page.
         </p>
+        <Dice />
         <Link
           to="roller"
           className="bg-transparent text-lg text-yellow-500 py-2 px-4 border border-yellow-700 rounded"
