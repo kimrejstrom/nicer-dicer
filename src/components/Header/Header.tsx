fbbd679c3f094cb2e40e8eecbdd367ed014440fe
Kim Rejstrom <kim.rejstrom@futurice.com>
2020-01-18 20:48:58 +0200

    Add link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import d20 from 'images/logo.svg';
+import { Link } from 'react-router-dom';
 
 export const Header: React.FC = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center">
         <div className="w-full absolute top-0 h-1 bg-yellow-700"></div>
-        <h1 className="text-center mt-4 mr-12">Nicer</h1>
+        <h1 className="text-center mt-4 mr-6">Nicer</h1>
         <div className="absolute arrow-down shape-shadow"></div>
-        <img
-          src={d20}
-          className="absolute top-0 w-12 px-2 py-2 shape-shadow"
-          alt="logo"
-        />
-        <h1 className="text-center mt-4 ml-12">Dicer</h1>
+        <Link className="w-12" to="/roller">
+          <img
+            src={d20}
+            className="absolute top-0 w-12 px-2 py-2 shape-shadow"
+            alt="logo"
+          />
+        </Link>
+        <h1 className="text-center mt-4 ml-6">Dicer</h1>
       </div>
     </div>
   );
