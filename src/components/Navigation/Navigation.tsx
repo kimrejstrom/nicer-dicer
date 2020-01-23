21ea42499cd689e5717f699647702fddf5270d47
Kim Rejstrom <kim.rejstrom@futurice.com>
2020-01-23 16:55:50 +0200

    increase touch area

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,14 +5,17 @@ import d20 from 'images/logo.svg';
 export const Navigation: React.FC = () => {
   return (
     <header className="h-20 fixed bottom-0 w-full border-t-2 border-yellow-700 bg-secondary-dark">
-      <nav className="flex align-center items-center justify-between flex-wrap p-4">
-        <Link to="/" className="mx-4">
+      <nav className="flex align-center items-center justify-between flex-wrap px-4">
+        <Link to="/" className="p-4">
           <img src="https://icon.now.sh/home/24/fefcbf" alt="home" />
         </Link>
-        <Link to="/roller" className="font-semibold text-xl tracking-tighter">
+        <Link
+          to="/roller"
+          className="font-semibold text-xl tracking-tighter p-4"
+        >
           <img src={d20} className="w-8" alt="logo" />
         </Link>
-        <Link to="/about" className="mx-4">
+        <Link to="/about" className="p-4">
           <img src="https://icon.now.sh/info_outline/24/fefcbf" alt="info" />
         </Link>
       </nav>
